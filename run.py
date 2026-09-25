0b4a0fe5749cc46606edb8e3641690955d386e85
Ivon-0810 <myvongauthier@gmail.com>
2026-09-25 11:25:33 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,13 @@
-from app import create_app
-from app.extensions import db
-from app.data.seed_data import peupler_base
-from app.data.generateur_joueurs import generer_effectifs
+import webbrowser
+from threading import Timer
+from app import create_app  # Ou la fonction/variable que tu utilises pour initialiser ton app Flask
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()
-    peupler_base()
-    generer_effectifs()
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:5000/')
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    # Ouvre le navigateur au bout de 1.5 seconde (le temps que Flask démarre)
+    Timer(1.5, open_browser).start()
+    app.run(host='127.0.0.1', port=5000, debug=False)
